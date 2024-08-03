d21fa12ab7e26ef367a8e7a6210f5aac5f562162
Alfredo Navarro <anavarrolga@gmail.com>
2024-08-03 15:27:14 -0600

    Screenshots for git

--- src/views/Detail/Detail.tsx
+++ src/views/Detail/Detail.tsx
@@ -26,7 +26,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 16,
-        backgroundColor: 'rgba(7,26,93,255)',
+        backgroundColor: 'rgba(7,26,93,255)', // color azul nasa
     },
     content: {
         backgroundColor: '#2c449d',
